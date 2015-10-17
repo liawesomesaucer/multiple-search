f3cf3d62a73971ecf44dbfb12fc3d542321baf10
Vincent <vincent.oddrepublic@gmail.com>
2015-10-16 18:50:58 -0700

    Yes colors

---
+++ colors.js
@@ -0,0 +1,59 @@
+// Color schemes.
+// Pick one of the dank implemented color schemes
+color_scheme = "default_blue_accent";
+
+var colors = {
+
+	"dank_blue" : {
+		"main-color" : "#006666",
+		"background-color" : "#33ffff",
+		"input-background-color" : "#00cccc",
+		"input-background-hover" : "#00b2b2",
+		"banner-color": "#fff"
+	},
+
+	"default_grayscale" : {
+		"main-color" : "#666",
+		"background-color" : "#fff",
+		"input-background-color" : "#e6e6e6",
+		"input-background-hover" : "#8c8c8c",
+		"banner-background-color": "#e6e6e6"
+	},
+
+	"default_blue_accent" : {
+		"main-color" : "#006666",
+		"background-color" : "#e6e6e6",
+		"input-background-color" : "#d2d2d2",
+		"input-background-hover" : "#8c8c8c",
+		"banner-background-color": "#e6e6e6"
+	},
+
+	"default_dark" : {
+		"main-color" : "#222",
+		"background-color" : "#555",
+		"input-background-color" : "#bbbbbb",
+		"input-background-hover" : "#666",
+		"banner-background-color": "#bbbbbb"
+	},
+
+	"calm_green" : {
+		"main-color" : "rgba(0, 0, 0, 0.5)",
+		"background-color" : "#2dcb89",
+		"input-background-color" : "rgba( 0, 0, 0, 0.25 )",
+		"input-background-hover" : "rgba( 0, 0, 0, 0.8 )",
+		"banner-background-color": "rgba( 255, 255, 255, 0.8 )"
+	}
+}
+
+
+display_color_switcher = false;
+
+function change_colors( color_scheme ) {
+	color_settings = colors[color_scheme];
+	$( "*" ).css({ "color": color_settings["main-color"]});
+	$( ".wrapper" ).css({ "background-color":color_settings["background-color"] });
+	$( "input" ).css({ "background-color": color_settings["input-background-color"]});
+	$( ".banner" ).css({ "background-color": color_settings["banner-background-color"]});
+}
+
+change_colors( color_scheme );
--- formhandler.js
+++ formhandler.js
@@ -1,15 +1,18 @@
+// This page handles the construction and listening of the forms
+
 // This is the list of strings you should add to.
 // This list includes some pre-found ones. Uncomment them for them to show up.
 // You can also change the order (the page order will reflect the order of this "list")
 // Format: 
 //			name-you-want-displayed: url-of-search-path
-//	* I'll build an auto search-path-generator sometime soon but for now here.
+//
+// You can also move them around to change their orders
 var url_strings = {
 	"Bing": "https://bing.com/search?q=",
 	"Google": "https://google.com/#q=",
-	"Google Images": "https://www.google.com/search?site=&tbm=isch&q=",
+	// "Google Images": "https://www.google.com/search?site=&tbm=isch&q=",
 	"Wikipedia": "https://en.wikipedia.org/wiki/Special:Search?search=",
-	// "YouTube":"https://www.youtube.com/results?search_query=",
+	"YouTube":"https://www.youtube.com/results?search_query=",
 	// "Yahoo": "https://search.yahoo.com/search?p=",
 	// "KhanAcademy": "https://www.khanacademy.org/search?page_search_query=",
 	// "Ted": "http://www.ted.com/search?q=",
@@ -23,52 +26,49 @@ var url_strings = {
 	// "Flickr": "https://www.flickr.com/search/?text=",
 }
 
-	// Loading strings from JSON file
-	var url_strings
+// If you're looking at this you probably know what you're doing so...GET CRACKIN AND MAKE IT EVEN BETTER :)))))
+var msg_form_inc = " bar contains no input. Please check again";
+var error = false;
+var setup_string = '';
 
-	// If you're looking at this you probably know what you're doing so...GET CRACKIN AND MAKE IT EVEN BETTER :)))))
-	var msg_form_inc = " bar contains no input. Please check again";
-	var error = false;
-	var setup_string = '';
+function setup_and_listen( url_strings ) {
+	setup( url_strings );
+	listen( url_strings );
+}
 
-	function setup_and_listen( url_strings ) {
-		setup( url_strings );
-		listen( url_strings );
-	}
+// Builds the html page based on url_strings
+function setup( url_strings ) {
+	jQuery.each( url_strings, function(key, val) {
+		setup_string = setup_string + 
+		'<li class="li-form-elem">' + 
+			'<form class="form-elem" id="'+key+'">' + 
+				'<input class="inline input-box" id="'+key+'-input" type="text" name="'+key+'" placeholder="'+key+'" autofocus>' + 
+				'<input class="inline input-submit" id="'+key+'-submit" type="submit" value="&rarr;" tabindex="-1">' + 
+			'</form>' +
+		'</li>';
+	});
+	$( '.inner-ul' ).html( setup_string );
+}
 
-	// Builds the html page based on url_strings
-	function setup( url_strings ) {
-		jQuery.each( url_strings, function(key, val) {
-			setup_string = setup_string + 
-			'<li class="li-form-elem">' + 
-				'<form class="form-elem" id="'+key+'">' + 
-					'<input class="inline input-box" id="'+key+'-input" type="text" name="'+key+'" placeholder="'+key+'" autofocus>' + 
-					'<input class="inline input-submit" type="submit" value="&rarr;" tabindex="-1">' + 
-				'</form>' +
-			'</li>';
+// Listens for a submit event from any of the forms
+function listen( url_strings ) {
+	jQuery.each( url_strings, function(key, val) {
+		$( "#" + key ).submit( function( ev ) {
+			ev.preventDefault();
+		    if ( $( "#" + key + "-input" ).val() )
+		    	window.location=val + $( "#" + key + "-input" ).val();
+		    else display_banner( key, error=true, msg_form_inc );
 		});
-		$( '.inner-ul' ).html( setup_string );
-	}
+	});
+}
 
-	// Listens for a submit event from any of the forms
-	function listen( url_strings ) {
-		jQuery.each( url_strings, function(key, val) {
-			$( "#" + key ).submit( function( ev ) {
-				ev.preventDefault();
-			    if ( $( "#" + key + "-input" ).val() )
-			    	window.location=val + $( "#" + key + "-input" ).val();
-			    else display_banner( key, error=true, msg_form_inc );
-			});
-		});
-	}
+// Displays messages
+function display_banner( key, error, message ) {
+	if ( error ) 
+		$( ".banner" ).css( { "background-color": "#fff"});
+	$( "#banner-msg" ).text( key + message );
 
-	// Displays the error message
-	function display_banner( key, error, message ) {
-		if ( error ) 
-			$( ".banner" ).css( { "background-color": "#fff"});
-		$( "#banner-msg" ).text( key + message );
+	error = false;
+}
 
-		error = false;
-	}
-
-	setup_and_listen( url_strings );
+setup_and_listen( url_strings );
